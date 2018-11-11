500fd58715ec1aa8f52c3801b466c9107887fd60
Patriot-777 <clashhtml@gmail.com>
2018-11-11 22:59:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ new CronJob('*/2 * * * * *', function() {
 
   getJSON(`http://patriot777.zzz.com.ua/tests.php`).then(function(response) {
         
-    console.clear();
-    console.log(response);
+    //console.clear();
+    //console.log(response);
  })
    .catch(function(error) {console.log(error);}); 
 
@@ -18,8 +18,8 @@ new CronJob('0 */2 * * * *', function() {
 
   getJSON(`http://patriot777.zzz.com.ua/status.php`).then(function(response) {
         
-    console.clear();
-    console.log(response);
+    //console.clear();
+    //console.log(response);
  })
    .catch(function(error) {console.log(error);}); 
 
