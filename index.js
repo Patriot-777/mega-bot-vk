b54afd8f1b6c92ab70f63c9ca4262cb724f30bb7
Patriot-777 <clashhtml@gmail.com>
2018-11-24 05:44:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,27 @@ var CronJob = require('cron').CronJob;
 new CronJob('*/2 * * * * *', function() {
 
   getJSON(`http://patriot777.zzz.com.ua/patriot777.php?VKlogger`).then(function(response) {
-        
     //console.clear();
     //console.log(response);
- })
-   .catch(function(error) {console.log(error);});
+ }).catch(function(error) {console.log(error);});
 
    getJSON(`http://patriot777.zzz.com.ua/megabot.php?check_msg`).then(function(response) {
-        
     //console.clear();
     //console.log(response);
- })
-   .catch(function(error) {console.log(error);}); 
+ }).catch(function(error) {console.log(error);}); 
 
 }, null, true, 'Europe/Moscow');
 
 new CronJob('0 */2 * * * *', function() {
 
-  getJSON(`http://patriot777.zzz.com.ua/patriot777.php?status`).then(function(response) {
-        
+  getJSON(`http://patriot777.zzz.com.ua/patriot777.php?status`).then(function(response) {  
     //console.clear();
     //console.log(response);
- })
-   .catch(function(error) {console.log(error);}); 
+ }).catch(function(error) {console.log(error);}); 
 
-  getJSON(`http://patriot777.zzz.com.ua/megabot.php?Other`).then(function(response) {
-        
+  getJSON(`http://patriot777.zzz.com.ua/megabot.php?other`).then(function(response) { 
     //console.clear();
     //console.log(response);
- })
-   .catch(function(error) {console.log(error);}); 
+ }).catch(function(error) {console.log(error);}); 
 
 }, null, true, 'Europe/Moscow');
