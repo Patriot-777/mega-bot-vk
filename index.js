b1a0ff918ca230ee210c7daac88b2bcc91804fcd
Yaroslav Andreev <clashhtml@gmail.com>
2019-01-18 23:34:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
-
 var getJSON = require('get-json');
 
 var CronJob = require('cron').CronJob;
 
-new CronJob('*/2 * * * * *', function() {
+new CronJob('*/4 * * * * *', function() {
 
-  getJSON(`https://tanktastic.000webhostapp.com/patriot777.php?heroku&VKlogger`).then(function(response) {
+  getJSON(`https://yaroslav-andreev.ru/megabot/megabot.php?heroku&send_msg`).then(function(response) {
     //console.clear();
     //console.log(response);
  }).catch(function(error) {console.log(error);});
 
-   getJSON(`https://tanktastic.000webhostapp.com/megabot.php?heroku&check_msg`).then(function(response) {
+ getJSON(`https://yaroslav-andreev.ru/patriot777/patriot777.php?heroku&VKlogger`).then(function(response) {
+    //console.clear();
+    //console.log(response);
+ }).catch(function(error) {console.log(error);});
+
+   getJSON(`https://yaroslav-andreev.ru/megabot/megabot.php?heroku&check_msg`).then(function(response) {
     //console.clear();
     //console.log(response);
  }).catch(function(error) {console.log(error);}); 
-
-}, null, true, 'Europe/Moscow');
-
-new CronJob('*/4 * * * * *', function() {
-
-  getJSON(`https://tanktastic.000webhostapp.com/megabot.php?heroku&send_msg`).then(function(response) {
-    //console.clear();
-    //console.log(response);
- }).catch(function(error) {console.log(error);});
 
 }, null, true, 'Europe/Moscow');
 
 new CronJob('0 */2 * * * *', function() {
 
-  getJSON(`https://tanktastic.000webhostapp.com/patriot777.php?heroku&status`).then(function(response) {  
+  getJSON(`https://yaroslav-andreev.ru/patriot777/patriot777.php?heroku&status`).then(function(response) {  
     //console.clear();
     //console.log(response);
  }).catch(function(error) {console.log(error);}); 
 
-  getJSON(`https://tanktastic.000webhostapp.com/megabot.php?heroku&other`).then(function(response) { 
+  getJSON(`https://yaroslav-andreev.ru/megabot/megabot.php?heroku&other`).then(function(response) { 
     //console.clear();
     //console.log(response);
  }).catch(function(error) {console.log(error);}); 
 
-}, null, true, 'Europe/Moscow');
-
-new CronJob('10 */2 * * * *', function() {
-
-  getJSON(`https://testvkapi-patriot777.000webhostapp.com/update.php?heroku`).then(function(response) { 
+ getJSON(`https://yaroslav-andreev.ru/megabot/update.php?heroku`).then(function(response) { 
     //console.clear();
     //console.log(response);
  }).catch(function(error) {console.log(error);}); 
